6cda2e7b8e5d76fad709adbc6629c4c4d9914f8a
Jake Pelrah <pelrahjsp@gmail.com>
2024-08-26 19:44:36 -0400

    refactor

--- main.js
+++ main.js
@@ -1,31 +1,27 @@
+
+/////////////////////// Part 1 ///////////////////////
 const radius = 5
-const area = 3.1415 * radius ** 2
+const area = 3.1415 * radius * radius
 const minSpacePerPlant = 0.8
 const numPlantsToStart = 20
 
 // can't have a fraction of a plant
 const maxCapacity = Math.floor(area / minSpacePerPlant)
 
+console.log(area, minSpacePerPlant, maxCapacity)
 
 function predictGrowth(numWeeks) {
 
     // number of plants doubles every week
-    const numPlants = numPlantsToStart * numWeeks ** 2
+    const numPlants = numPlantsToStart * 2 ** numWeeks
 
+    // can't have a fraction of a plant
     const eightyPercentOfMax = Math.floor(maxCapacity * .80)
     const fiftyPercentOfMax = Math.floor(maxCapacity * .50)
 
-
-    //pruned
-    if (numPlants > eightyPercentOfMax) {
-        console.log(`
-Week: ${numWeeks}
-Number of plants: ${numPlants} 
-Eighty % of maxCapacity: ${eightyPercentOfMax} 
-Result: pruned\n`)
-    }
+    console.log(numPlants, fiftyPercentOfMax, eightyPercentOfMax)
     //monitored
-    else if (numPlants > fiftyPercentOfMax
+    if (numPlants >= fiftyPercentOfMax
         && numPlants <= eightyPercentOfMax) {
         console.log(`
 Week: ${numWeeks}
@@ -34,8 +30,18 @@ Fifty % of maxCapacity: ${fiftyPercentOfMax}
 Eighty % of maxCapacity: ${eightyPercentOfMax}
 Result: monitored\n`)
     }
+
+    //pruned
+    else if (numPlants > eightyPercentOfMax) {
+        console.log(`
+Week: ${numWeeks}
+Number of plants: ${numPlants} 
+Eighty % of maxCapacity: ${eightyPercentOfMax} 
+Result: pruned\n`)
+    }
+
     //planted
-    else if (numPlants <= fiftyPercentOfMax) {
+    else if (numPlants < fiftyPercentOfMax) {
         console.log(`
 Week: ${numWeeks}
 Number of plants: ${numPlants} 
@@ -44,7 +50,8 @@ Result: planted\n`)
     }
 }
 
-
+predictGrowth(0)
 predictGrowth(1)
 predictGrowth(2)
 predictGrowth(3)
+
